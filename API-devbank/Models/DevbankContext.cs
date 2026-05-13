c72793ac8e0e69da1d2296601a2ced9bd2dca4d0
gabrielDuarte <biel111107@gmail.com>
2026-05-13 19:27:17 -0300

    remove localhost from dbcontext

--- API-devbank/Models/DevbankContext.cs
+++ API-devbank/Models/DevbankContext.cs
@@ -24,9 +24,6 @@ public partial class DevbankContext : DbContext
 
     public virtual DbSet<TabelaChavePix> TabelaChavePix { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;database=devbank;user=root;password=senai", Microsoft.EntityFrameworkCore.ServerVersion.Parse("12.0.2-mariadb"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
